577b78daa62bef3ef0210866c00d1c46d499855e
sikha22 <84958102+sikha22@users.noreply.github.com>
2023-01-26 10:59:42 +0530

    Create MinimumDifferenceinanArray.cpp

---
+++ Sorting/MinimumDifferenceinanArray.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    //Function to find minimum difference between any pair of elements in an array.
+    int MinimumDifference(int arr[], int n)
+    {
+        //code here
+        int minm=INT_MAX;
+       sort(arr,arr+n);
+       for(int i=1;i<n;i++){
+           minm=min(minm,arr[i]-arr[i-1]);
+       }
+       return minm;
+    }
+};
+
+//{ Driver Code Starts.
+    
+int main() {
+	
+	int t;
+	cin>>t;
+	while(t--){
+	    int n; cin>>n;
+	    int arr[n]; 
+	    for(int i = 0; i < n; i++){
+	        cin>>arr[i];
+	    }
+	    Solution ob;
+	    int diff = ob.MinimumDifference(arr, n);
+	    cout << diff << endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
