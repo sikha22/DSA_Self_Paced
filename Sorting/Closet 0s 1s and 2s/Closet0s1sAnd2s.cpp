37b3ae60f61d54d81ecffefb9d5eb6bdca15bce9
Sikha Kumari <84958102+sikha22@users.noreply.github.com>
2023-02-05 11:39:51 +0530

    Create Closet0s1sAnd2s.cpp

---
+++ Sorting/Closet 0s 1s and 2s/Closet0s1sAnd2s.cpp
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    
+    // The function should do the stated modifications in the given array arr[]
+    // Do not return anything.
+    
+    // arr[]: Input Array
+    // N: Size of the Array arr[]
+    //Function to segregate 0s, 1s and 2s in sorted increasing order.
+    void segragate012(int arr[], int N)
+    {
+        // Your Code Here
+        vector<int> nums(3, 0);
+        for(int i = 0; i < N; i++) {
+               nums[arr[i]]++;
+            }
+   
+   int p = 0;
+   for(int i = 0; i < 3; i++) {
+       while(nums[i]--) {
+           arr[p] = i;
+           p++;
+       }
+   }
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+
+    int T;
+    cin >> T;
+
+    while(T--){
+        int N;
+        cin >>N;
+        int A[N];
+        for(int i=0;i<N;i++){
+            cin >> A[i];
+        }
+
+        Solution ob;
+        ob.segragate012(A, N);
+
+        for(int i=0;i<N;i++){
+            cout << A[i]  << " ";
+        }
+
+        cout << endl;
+        
+        
+    }
+
+}
+
+// } Driver Code Ends
