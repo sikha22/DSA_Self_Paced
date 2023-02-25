03f16a7a6704dbc739fcacbebf88b7a44344bf97
Sikha Kumari <84958102+sikha22@users.noreply.github.com>
2023-02-25 10:38:40 +0530

    Create Count More than n,k Occurences.cpp

---
+++ Searching/Count More than n,k Occurences.cpp
@@ -0,0 +1,17 @@
+
+class Solution
+{
+    public:
+    //Function to find all elements in array that appear more than n/k times.
+    int countOccurence(int arr[], int n, int k) {
+        // Your code here
+         unordered_map<int,int> mp;
+       for(int i=0;i<n;i++)mp[arr[i]]++;
+       int ans=0;
+       for(auto it=mp.begin();it!=mp.end();it++){
+           if(it->second > n/k)
+               ans++;
+       }
+       return ans;
+    }
+};
